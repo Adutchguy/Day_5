ffd3c47f2c4f9108ba32e7c648a1db8fe0728888
Adutchguy <mdm_8@hotmail.com>
2017-03-11 00:19:05 -0800

    Solved problem 4

--- lab/app.js
+++ lab/app.js
@@ -60,7 +60,7 @@ function sumAndMultiply(a,b,c){ //eslint-disable-line
   var sumString =  a + ' and ' + b + ' and ' + c + ' sum to ' + numberSum1[0] + '.';
   var productString = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + numberProduct2[0] + '.';
 
-  return [numberSum1,sumString,productString,numberProduct2];
+  return [numberSum1[0],sumString,productString,numberProduct2[0]];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
@@ -82,12 +82,18 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2,3,4]; //eslint-disable-line
 
 function sumArray(testArray){ //eslint-disable-line
+  var firstTwoSum1 = sum(testArray[0],testArray[1]);
+  var numberSum2 = sum(firstTwoSum1[0],testArray[2]);
 
+  var sumArrayString = testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' was passed in as an array of numbers, and ' + numberSum2[0] + ' is their sum.';
+  console.log(sumArrayString);
+
+  return [numberSum2[0],sumArrayString];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
